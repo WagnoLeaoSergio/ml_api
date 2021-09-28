4d48383da0e1325d935b3fd16f33daf660c60a9c
WagnoLeaoSergio <wagnoleao@gmail.com>
2021-09-28 17:09:19 -0300

    Criacao de um banco de dados SQLite e integracao da API via ORM

--- ml_api/__init__.py
+++ ml_api/__init__.py
@@ -2,17 +2,31 @@ import os
 import json
 from datetime import datetime
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
 load_dotenv()
 APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///measures.sqlite3"
+
+db = SQLAlchemy(APP)
 
 #os.getenv('VARIABLE')
 
-@APP.route('/')
-def index():
-    return '<h1>Index Page</h1>'
+class Measure(db.Model):
+    """
+    Classe que representa a tabela no banco de dados das medidas registradas.
 
+    Parameter
+    --------
+
+    id (int): Id da medida (chave-primaria).
+    medida (float): Valor registrado pelo sensor.
+    timestamp (DateTime): Data em que o valor foi registrado na API. 
+    """
+    id = db.Column('id', db.Integer, primary_key=True)
+    medida = db.Column('medida', db.Float)
+    timestamp = db.Column('timestamp', db.DateTime)
 
 @APP.route('/hello')
 def hello_world():
@@ -21,30 +35,42 @@ def hello_world():
 @APP.route('/sensor', methods=['POST'])
 def sensor():
     """
-    Rota que recebe um valor do sensor de frequência cardíaca e armazena no banco de dados.
+    Rota que recebe dados de frequência cardíaca e armazena no banco de dados.
+
+    Converte o valor para um número que (caso fornecido) e faz a devidas validações.
+
+    Retorna uma mensagem de erro específica em caso de falha.
 
     Arguments
     ----------
-    measure (int, float): - Valor que será armazenado.
+    medida (str): Valor que será armazenado.
 
     Returns
     ---------
     JSON (str): JSON com o resultado da operação.
     """
-    measure = request.form['measure']
+     
     current_date = datetime.now().strftime("%d/%m/%Y:%H:%M:%S")
-    
     result_object = {
         "result": None,
         "error": None,
         "timestamp": current_date 
     }
 
-    if not measure.replace('.', '', 1).isdigit():
-        result_object["error"] = "O Valor não é um número."
+    if not 'medida' in request.form:
+        result_object["error"] = "O valor 'medida' não foi fornecido."
     else:
-        result_object["result"] = f"O valor {request.form['measure']}" \
-        f" foi registrado na data {current_date}."
+        medida = request.form['medida']
+
+        if not medida.replace('.', '', 1).isdigit():
+            result_object["error"] = "O Valor não é um número."
+        else:
+            measure = Measure(medida=float(medida), timestamp=datetime.now())
+            db.session.add(measure)
+            db.session.commit()
+
+            result_object["result"] = f"O valor {request.form['medida']}" \
+            f" foi registrado."
 
     return json.dumps(
             result_object,
